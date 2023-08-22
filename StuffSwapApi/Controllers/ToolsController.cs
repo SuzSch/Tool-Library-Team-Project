174e22de064d6fdf6b3189c35b4c23a6b72ef80b
Daniel Kiss <flowfast47@gmail.com>
2023-08-22 11:06:48 -0700

    add crud tools controller

--- StuffSwapApi/Controllers/ToolsController.cs
+++ StuffSwapApi/Controllers/ToolsController.cs
@@ -29,6 +29,73 @@ namespace StuffSwapApi.Controllers
       return await _db.Tools.ToListAsync();
     }
 
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Tool>> GetTool(int id)
+    {
+      Tool tool = await _db.Tools.FindAsync(id);
+      if (tool == null)
+      {
+        return NotFound();
+      }
+      return tool;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Post(Tool tool)
+    {
+      await _db.Tools.AddAsync(tool);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction(nameof(GetTool), new { id = tool.ToolId }, tool);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Tool>> Put(int id, Tool tool)
+    {
+      if (id != tool.ToolId)
+      {
+        return BadRequest();
+      }
+      // the async part is not the update but the save
+      _db.Tools.Update(tool);
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!ToolExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      //204 code in empty object shows success
+      return NoContent();
+    }
+
+    private bool ToolExists(int id)
+    {
+      return _db.Tools.Any(e => e.ToolId == id);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Tool>> DeleteTool(int id)
+    {
+      Tool deleteTool = await _db.Tools.FindAsync(id);
+      if (deleteTool == null)
+      {
+        return NotFound();
+      }
+      _db.Tools.Remove(deleteTool);
+      await _db.SaveChangesAsync();
+      return NoContent();
+    }
+
 
   }
 }
